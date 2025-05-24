27dbad72b0ea3bf94ee1a228deb702e12b3d9ec4
yfe404 <yfe.github@protonmail.com>
2025-05-24 18:33:26 +0200

    feat: (wip) add RAG

--- telegram-bot/agent.py
+++ telegram-bot/agent.py
@@ -9,16 +9,24 @@ from typing import List
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
+from langchain import hub
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.vectorstores import InMemoryVectorStore
+
 openai_api_key = os.environ.get('OPENAI_API_KEY')
 
+
+
+
 @tool
-def get_user_infos(user_id: str) -> str:
+def get_user_infos(userid: str) -> str:
     """
     Returns the infos from 42 intra about a specific user.
 
     Rreturns string of user data
     """
-    response = requests.get("http://api-wrapper:5000/connected")
+    response = requests.get(f"http://api-wrapper:5000/user/{userid}")
 
 
 @tool
@@ -45,6 +53,14 @@ model = ChatOpenAI(
 
 class Agent:
     def __init__(self):
+        pages = []
+        for file_path in ['/docs/test.pdf', '/docs/test1.pdf']:
+            loader = PyPDFLoader(file_path)
+            for page in loader.lazy_load():
+                pages.append(page)
+
+        self.vector_store = InMemoryVectorStore.from_documents(pages, OpenAIEmbeddings())
+
         self.config = {"configurable": {"thread_id": "abc123"}}
         self.tools = [fetch_connected_users, get_user_infos]
         self.model = model
@@ -56,10 +72,24 @@ class Agent:
         )
 
     def get_response(self, query: str):
+        # api_url="https://api.smith.langchain.com" in hub.pull.
+        prompt = hub.pull("rlm/rag-prompt")
+        retrieved_docs = self.vector_store.similarity_search(query)
+        docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+        prompt = prompt.invoke({"question": query, "context": docs_content})
+
+        prompt = f"""
+            QUESTION:
+            {query}
+            --------------------------
+            CONTEXT (if needed):
+            {docs_content}
+        """
+
         response = self.agent_executor.invoke(
                 {
                     "messages": [
-                        HumanMessage(content=query)
+                        HumanMessage(content=prompt)
                     ]}, self.config)
         print(response)
         return response["messages"][-1].content
